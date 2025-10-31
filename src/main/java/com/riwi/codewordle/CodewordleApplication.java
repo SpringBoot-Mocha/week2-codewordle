74b354476edab09f4f53d687baacb029a7b91c24
Johan192004 <johanruma@gmail.com>
2025-10-31 13:54:36 -0500

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/riwi/codewordle/CodewordleApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.codewordle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewordleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodewordleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/codewordle/CodewordleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.codewordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewordleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
